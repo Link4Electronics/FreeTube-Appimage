ebb91f589a4cbcf26e42d94a3292983eb9cf9a03
Link4Electronics <link4electronics@gmail.com>
2026-02-01 03:54:06 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -3,17 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
+DEB_LINK="https://github.com/FreeTubeApp/FreeTube/releases/download/v0.23.13-beta/freetube_0.23.13_beta_$ARCH.deb"
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm electron34
+#pacman -Syu --noconfirm
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package freetube
+#make-aur-package freetube
 
 # If the application needs to be manually built that has to be done down here
 
@@ -24,3 +25,24 @@ make-aur-package freetube
 # else
 # 	regular build steps
 # fi
+
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+if ! wget --retry-connrefused --tries=30 "$DEB_LINK" -O /tmp/app.deb 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
+
+ar xvf /tmp/app.deb
+tar -xvf ./data.tar.gz
+rm -f ./*.gz
+rm -rf ./usr/doc
+mv -v ./usr ./AppDir
+mv -v ./opt ./AppDir/lib
+mkdir ./AppDir/bin
+ln -s ./AppDir/lib/Freetube/freetube ./AppDir/bin/freetube
+cp -v ./AppDir/share/applications/freetube.desktop            ./AppDir
+cp -v ./AppDir/share/icons/hicolor/scalable/apps/freetube.svg  ./AppDir/.DirIcon
+cp -v ./AppDir/share/icons/hicolor/scalable/apps/freetube.svg  ./AppDir
+
+awk -F'/' '/Location:/{print $(NF-1); exit}' /tmp/download.log > ~/version
