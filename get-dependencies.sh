3b46c3a32f256c0464cc7d9a670dbff527b8b3da
Link4Electronics <link4electronics@gmail.com>
2026-02-05 12:55:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -24,7 +24,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package freetube
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
 
